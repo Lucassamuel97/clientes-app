cff56ccb3db8554b8226cb1f374e23c7861e312c
Lucassamuel97 <lukassamuka88@gmail.com>
2024-02-09 14:33:57 -0300

    Cadastro cliente

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Cliente } from '../cliente';
+import { ClientesService } from '../../clientes.service';
 
 @Component({
   selector: 'app-clientes-form',
@@ -10,16 +11,26 @@ import { Cliente } from '../cliente';
 export class ClientesFormComponent implements OnInit {
 
   cliente: Cliente;
+  success: boolean = false;
+  errors: String[];
 
-  constructor() {
+  constructor(private service: ClientesService) {
     this.cliente = new Cliente();
+    this.errors = [];
   }
 
   ngOnInit(): void {
   }
 
   onSubmit(){
-    console.log(this.cliente);
+    this.service.salvar(this.cliente).subscribe(response => {
+      this.success = true;
+      this.errors = [];
+      this.cliente = response;
+    }, errorResponse => {
+      this.success = false;
+      this.errors = errorResponse.error.errors;
+    });
   }
 
   clicar(){
