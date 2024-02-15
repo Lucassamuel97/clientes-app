9518c4f453618f89fd224d16c6d7f57f7ff427a1
Lucassamuel97 <lukassamuka88@gmail.com>
2024-02-15 16:33:56 -0300

    listagem dos servicos

--- src/app/servico-prestado/servico-prestado-lista/servico-prestado-lista.component.ts
+++ src/app/servico-prestado/servico-prestado-lista/servico-prestado-lista.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ServicoPrestadoBusca } from './servicoPrestadoBusca';
+import { ServicoPrestadoService } from '../../servico-prestado.service';
 
 @Component({
   selector: 'app-servico-prestado-lista',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './servico-prestado-lista.component.css'
 })
 export class ServicoPrestadoListaComponent {
+  nome: string = '';
+  mes!: number;
+  meses: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  lista: ServicoPrestadoBusca[] = [];
+  message!: boolean;
 
+  constructor(private service: ServicoPrestadoService){}
+
+  consultar() {
+    this.service.buscar(this.nome, this.mes)
+      .subscribe(response =>{ 
+        this.lista = response
+        if(this.lista.length <= 0){
+          this.message = true;
+        }else{
+          this.message = false;
+        }
+      });
+
+  } 
 }
